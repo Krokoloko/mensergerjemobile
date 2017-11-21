a89de5fdc48ccbae1685413b5bd879e9832bdf53
Daan Ruting <daan.j.ruting@gmail.com>
2017-11-21 11:25:45 +0100

    adding ui

---
+++ mensergerjemobile/Assets/logs/DaansLog.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DaansLog : MonoBehaviour {
+
+    ///<summary>
+    ///Daan Ruting log
+    ///
+    /// 20-11-2017 22:48
+    /// 
+    /// De eerste dag heb ik met Damion besloten dat we een mens erger je niet gingen maken voor de mobile.
+    /// Ik heb een git repository gecreërd en die meteen gedeelt me Damion. 
+    /// 
+    /// 
+    /// Ik heb een prototype uml gemaakt waar we aan kunnen houden.
+    /// Er liggen voornamelijk classes die we nog moeten creëren.
+    /// 
+    /// Vervolgens heb ik nog onderzoek gedaan hoe je de unity ios build op je telefoon kan testen.
+    /// Schijnt dat je of een virtual machine moet maken om de telefoons applicaties te kunnen beheren.
+    /// De andere optie is om het via een mac os erin te douwen.
+    /// 
+    /// 
+    ///
+    /// </summary>
+}
